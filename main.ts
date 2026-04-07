07d1f24d99c81d0e1dc825d6a6e2f47191b5de8e
senseiheather <heather.mckie@codeninjas.com>
2026-04-07 18:49:45 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,33 @@
+//% color="#AA278D" weight=100
+namespace physicsProperties {
 
+    /**
+     * @param jumpingSprite 
+     * @param jumpHeight
+     */
+    //% jumpingSprite.shadow="variables_get" jumpingSprite.defl="mySprite"
+    //% jumpHeight.defl=10
+
+    //% block="set $jumpingSprite to jump with $jumpHeight"
+    export function jump (jumpingSprite: Sprite, jumpHeight: number) {
+      if (jumpingSprite.isHittingTile(CollisionDirection.Bottom)) {
+          jumpingSprite.vy = jumpHeight * -1
+        }  
+    }
+
+  /**
+    * @param spriteForPhysics 
+    * @param gravity
+     */
+
+    //% spriteForPhysics.shadow="variables_get" spriteForPhysics.delf="mySprite"
+    //% gravity.delf=100
+    //% block="set gravity for $spriteForPhysics with a strength of $gravity"
+    export function applyGravity (spriteForPhysics: Sprite, gravity: number) {
+    spriteForPhysics.ay = gravity
+    }   
+
+
+
+  
+}
